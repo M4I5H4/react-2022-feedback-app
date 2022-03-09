4da2d8d27c106350539125e4cc1e934c5cb40756
M4I5HA <maisha.adamsam@gmail.com>
2022-03-09 22:36:43 +0000

    added prop stats component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
-import { useState } from "react";
-import Header from "./components/Header";
-import FeedbackList from "./components/FeedbackList";
-import FeedbackData from "./data/FeedbackData";
+import { useState } from 'react';
+import Header from './components/Header';
+import FeedbackList from './components/FeedbackList';
+import FeedbackData from './data/FeedbackData';
+import FeedbackStats from './components/FeedbackStats';
 
 function App() {
-  const [feedback, setFeedback] = useState(FeedbackData)
+  const [feedback, setFeedback] =
+    useState(FeedbackData);
+  //delete function is here because this is where the feedback data is (see prop drilling)
+  const deleteFeedback = (id) => {
+    if (
+      window.confirm(
+        'Are you sure you want to delete?'
+      )
+    ) {
+//sets array to the feedback list minus the item deleted
+      setFeedback(
+        feedback.filter((item) => item.id !== id)
+      );
+    }
+  };
   return (
     <>
-    <Header />
-    <div className="container">
-      <FeedbackList feedback={feedback} />
-    </div>
+      <Header />
+      <div className='container'>
+        <FeedbackStats feedback={feedback}/>
+        <FeedbackList
+          feedback={feedback}
+          handleDelete={deleteFeedback}
+        />
+      </div>
     </>
   );
 }
-
 
 export default App;
--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -2,11 +2,15 @@
 import PropTypes from "prop-types"
 //changed the card div to a card component
 import Card from "./shared/Card"
-function FeedbackItem({item}) {
+//added icon library npm i react-icons
+import{ FaTimes} from 'react-icons/fa'
+function FeedbackItem({item, handleDelete}) {
+
     
   return (
     <Card>
         <div className="num-display">{item.rating}</div>
+        <button onClick={() => handleDelete(item.id)} className="close"><FaTimes color="purple"/></button>
         <div className="text-display">{item.text}</div> 
     </Card>
   )
